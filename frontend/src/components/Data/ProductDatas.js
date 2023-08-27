b145f160abf1f5ef68bc0ba553d08d1890c7dc89
Rakesh <rakeshcse123@gmail.com>
2023-08-27 17:50:45 +0530

    Bug Fix

--- frontend/src/components/Data/ProductDatas.js
+++ frontend/src/components/Data/ProductDatas.js
@@ -20,20 +20,20 @@ const ProductDatas = [
         price: '999',
         rating: '4.8 *'
     },
-    {
-        id: 4,
-        name: 'Chocolate',
-        tag: 'https://butterry.com/image/cache/catalog/buttery/sq-choco-vanilla-cake0006chva-AA-1000x1000.jpg',
-        price: '399',
-        rating: '4.8 *'
-    },
-    {
-        id: 5,
-        name: 'Pineapple',
-        tag: 'https://assets.winni.in/product/primary/2022/9/73522.jpeg?dpr=1&w=400',
-        price: '599',
-        rating: '4.7 *'
-    },
+    // {
+    //     id: 4,
+    //     name: 'Chocolate',
+    //     tag: 'https://butterry.com/image/cache/catalog/buttery/sq-choco-vanilla-cake0006chva-AA-1000x1000.jpg',
+    //     price: '399',
+    //     rating: '4.8 *'
+    // },
+    // {
+    //     id: 5,
+    //     name: 'Pineapple',
+    //     tag: 'https://assets.winni.in/product/primary/2022/9/73522.jpeg?dpr=1&w=400',
+    //     price: '599',
+    //     rating: '4.7 *'
+    // },
 ]
 
 export default ProductDatas
